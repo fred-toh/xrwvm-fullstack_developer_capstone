620e59bbdaccf400c5f9e854ea5641f0908b132d
fred-toh <fredtoh@hotmail.com>
2024-07-14 03:34:36 -0400

    Corrections made on djangoapp files. Module 3c complete.

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -29,7 +29,7 @@ def get_request(endpoint, **kwargs):
         print("Network exception occurred")
 
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"/analyze/"+text
+    request_url = sentiment_analyzer_url+"analyze/"+text
     # print(request_url)
     try:
         # Call get method of requests library with URL and parameters
