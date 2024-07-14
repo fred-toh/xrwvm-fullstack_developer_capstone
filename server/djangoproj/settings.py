fa6ebac9a13e831828c47b83d36da929813b1043
fred-toh <fredtoh@hotmail.com>
2024-07-14 02:33:48 -0400

    Module 3c updated.

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,9 +30,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 
     'https://fredtoh-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://fredtoh-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'https://fredtoh-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = ['https://fredtoh-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://fredtoh-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'https://fredtoh-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
